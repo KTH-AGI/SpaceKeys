554597534fe27de85ee675c99fc7e01c40a47da3
JackyL56 <haijiejacky@gmail.com>
2023-11-20 13:18:04 +0100

    FIX: Bug for Server RPC calls
    Missing reference on Client side for DebugText. Fix: Remove DebugText.

--- Assets/Scripts/Networking/Player/RpcTestNew.cs
+++ Assets/Scripts/Networking/Player/RpcTestNew.cs
@@ -6,8 +6,6 @@ using Random = UnityEngine.Random;
 
 public class RpcTestNew : NetworkBehaviour
 {
-    [SerializeField] private TMP_Text DebugText;
-    
     [HideInInspector] // Otherwise, we get a Bug: it throws an InvalidCastException. Should be fixed when updating NGO to 1.7.0
     public NetworkVariable<Vector3> Position = new NetworkVariable<Vector3>();
     [HideInInspector] 
@@ -23,33 +21,16 @@ public class RpcTestNew : NetworkBehaviour
         }
     }
 
-    /*public void Start()
-    {
-        if (NetworkManager.Singleton.IsClient)
-        {
-            // Input.gyro.enabled = true;
-        }
-
-        if (NetworkManager.Singleton.IsServer)
-        {
-            _networkObject = GetComponent<NetworkObject>();
-            _networkObject.Spawn();
-        }
-    }*/
 
     [ServerRpc(RequireOwnership = false)]
     void SubmitPositionRequestServerRpc(ServerRpcParams rpcParams = default)
     {
         
-        // DebugText.text = "Inside if! " + Input.gyro.attitude;
-        
         // Get Gyro Rotation
         // Rotation.Value = Input.gyro.attitude;
-        // Debug.Log("Rotating: " + Rotation.Value);
         
         // Add Acceleration Code Here
         Position.Value = GetRandomPositionOnPlane();
-        DebugText.text += "   Updated Position ";
 
 
     }
@@ -67,10 +48,8 @@ public class RpcTestNew : NetworkBehaviour
 
     void UpdateMovement()
     {
-        DebugText.text += ("  |||  Update Transform  ||| ");
         if (NetworkManager.Singleton.IsClient)
         {
-            DebugText.text += " ||| Inside Submit Position  |||";
             SubmitPositionRequestServerRpc();
         }
     }
@@ -83,16 +62,14 @@ public class RpcTestNew : NetworkBehaviour
         // Todo add acceleration to modify position
         transform.position = Position.Value;
         
-        DebugText.text += ("  |||  Updated ||| ");
     }
 
     void Update()
     {
-        DebugText.text = "";
+        // DebugText.text = "";
         UpdateMovement();
         UpdateTransform();
 
         String tmp = "    IsClient: " + IsClient + " NetworkManager Client: " + NetworkManager.Singleton.IsClient + "  NServer: " + NetworkManager.Singleton.IsServer;
-        DebugText.text += tmp;
     }
 }
