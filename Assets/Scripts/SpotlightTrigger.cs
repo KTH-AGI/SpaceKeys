800ed2f0b259d11cdb47c9ea9f3006b779bdc6f4
JackyL56 <haijiejacky@gmail.com>
2023-11-27 12:28:23 +0100

    Merged SoundCue when notes are hit into SpotlightTrigger.

--- Assets/Scripts/SpotlightTrigger.cs
+++ Assets/Scripts/SpotlightTrigger.cs
@@ -4,7 +4,13 @@ public class SpotlightTrigger : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
+        GameObject note = other.gameObject;
         // todo do score logic
-        other.gameObject.SetActive(false);
+        note.SetActive(false);
+        var audioSource = note.GetComponent<AudioSource>();
+        
+        AudioSource.PlayClipAtPoint(audioSource.clip, Camera.main.transform.position);          
+        Debug.Log("Music object audio played");
+        
     }
 }
