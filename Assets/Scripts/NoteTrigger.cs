5a6bc978e76c439a66b266de0547e407692352da
JackyL56 <haijiejacky@gmail.com>
2023-11-30 23:47:25 +0100

    First Draft of Shield Hull

--- Assets/Scripts/NoteTrigger.cs
+++ Assets/Scripts/NoteTrigger.cs
@@ -11,13 +11,16 @@ public class NoteTrigger : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        GameObject note = other.gameObject;
-        // todo do score logic
-        note.SetActive(false);
-        var audioSource = note.GetComponent<AudioSource>();
-        
-        AudioSource.PlayClipAtPoint(audioSource.clip, Camera.main.transform.position);          
-        Debug.Log("Music object audio played");
+        if (other.gameObject.tag == "Note")
+        {
+            GameObject note = other.gameObject;
+            // todo do score logic
+            note.SetActive(false);
+            var audioSource = note.GetComponent<AudioSource>();
+            
+            AudioSource.PlayClipAtPoint(audioSource.clip, Camera.main.transform.position);          
+            Debug.Log("Music object audio played");
+        }
         
     }
 }
