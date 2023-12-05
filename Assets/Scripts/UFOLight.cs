9893c86c013b8aad5b5352265d2f3acff6d4a6d1
Saga Palmér <sagapa@kth.se>
2023-12-05 21:16:37 +0100

    Notes grey when missed, light disappear

--- Assets/Scripts/UFOLight.cs
+++ Assets/Scripts/UFOLight.cs
@@ -2,19 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NoteLight : MonoBehaviour
+public class UFOLight : MonoBehaviour
 {
     int gridSize = CircularGrid.gridSize;
     float radius = CircularGrid.radius;
 
-    [SerializeField] Material missedNoteMaterial;
     [SerializeField] Material lightMaterial;
     [SerializeField] float moveSpeed = 5.0f;
     [SerializeField] float lineWidth = 0.5f;
     [SerializeField] int zSpawn = 50;
     [SerializeField] int playerPosition = 25;
     GameObject circularLight;
-    Renderer renderer;
 
     //LineRenderer lineRenderer;
 
@@ -22,37 +20,12 @@ public class NoteLight : MonoBehaviour
     {
         //lineRenderer = gameObject.AddComponent<LineRenderer>();
         createRoundLight();
-        renderer = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float zPosition = transform.position.z;
-        if (zPosition < zSpawn && zPosition > playerPosition) //25 is the player position, need to change this later
-        {
-            circularLight.SetActive(true);
-            renderer.material = lightMaterial;
-            LineRenderer lineRenderer = circularLight.GetComponent<LineRenderer>();
-            for (int i = 0; i < gridSize; i++)
-            {
-                float angle = i * (360f / gridSize);
-                float x = Mathf.Cos(angle * Mathf.Deg2Rad) * radius;
-                float y = Mathf.Sin(angle * Mathf.Deg2Rad) * radius;
-
-
-                Vector3 point = new Vector3(x, y, zPosition);
-                lineRenderer.SetPosition(i, point);
-                if (i == 0) { lineRenderer.SetPosition(gridSize, point); }
-            }
-        }
-        else {
-            circularLight.SetActive(false);
-            if (zPosition < playerPosition) { 
-                renderer.material = missedNoteMaterial;
-            }
-        }
-        
+ 
     }
 
 
@@ -67,6 +40,8 @@ public class NoteLight : MonoBehaviour
         lineRenderer.endWidth = lineWidth;
         lineRenderer.positionCount = gridSize + 1;
 
+        float zPosition = transform.position.z;
+
         for (int i = 0; i < gridSize; i++)
         {
             float angle = i * (360f / gridSize);
@@ -74,11 +49,10 @@ public class NoteLight : MonoBehaviour
             float y = Mathf.Sin(angle * Mathf.Deg2Rad) * radius;
 
 
-            Vector3 point = new Vector3(x, y, 0);
+            Vector3 point = new Vector3(x, y, zPosition);
             lineRenderer.SetPosition(i, point);
             if (i == 0) { lineRenderer.SetPosition(gridSize, point); }
         }
-        circularLight.SetActive(false);
 
     }
 
