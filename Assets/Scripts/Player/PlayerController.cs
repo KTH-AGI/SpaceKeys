2bcd8eed2e2ceec34824afdf71cf3dfb717b7d16
xxpermanentxx <shiqi.gong.2000@gmail.com>
2023-12-08 18:57:03 +0100

    Hand tracking issue
    There is a bug in the hand tracking section. If the user always uses only one hand, MediaPipe sometimes thinks there are two. It will lead to "Object reference not set to an instance of an object" (see the image). If the user use two hand when the game start, it won't encounter this bug. But if Mario still use one hand it will be a error.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -89,8 +89,19 @@ public class PlayerController : MonoBehaviour
         if (initialized) return true;
 
         // Found hands! Initialize Lists and Screen Size
-        _handLandmarkListLeftHand = _handLandmarkListAnnotation[0];
-        _handLandmarkListRightHand = _handLandmarkListAnnotation[1];
+        for (int i = 0; i < 2; i++)
+        {
+            HandLandmarkListAnnotation hand = _handLandmarkListAnnotation[i];
+            if(hand.GetHandedness()=="left")
+            {
+                _handLandmarkListLeftHand = hand;
+            }
+            else if(hand.GetHandedness()=="right")
+            {
+                _handLandmarkListRightHand = hand;
+            }
+        }
+
         InitializeScreenDimensions();
         initialized = true;
         return true;
