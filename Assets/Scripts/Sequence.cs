0bddf9dec51f1c1ad88ddd2080a9f1a14c0bdd0f
Linnéa Gustafsson <linnea.gustafsson@hotmail.se>
2023-12-14 14:32:30 +0100

    Prepared whole song sequence (#128)
    Commented out the lines using objects that aren't available yet

--- Assets/Scripts/Sequence.cs
+++ Assets/Scripts/Sequence.cs
@@ -23,39 +23,76 @@ public class Sequence
         offset = (int)((timeOffset + additionalTimeOffset) / sixteenthTime);
 
         // Intro
-        //sequence[musicNotationToOffsetSample(3, 1)] = new MusicObjectInfo("StarBb2", 0.3f);
-        sequence[musicNotationToOffsetSample(9, 1)] = new MusicObjectInfo("StarBb2", 0.5f);
-        sequence[musicNotationToOffsetSample(13, 1)] = new MusicObjectInfo("StarDb3", 0.5f);
-        sequence[musicNotationToOffsetSample(17, 1)] = new MusicObjectInfo("StarF3", 0.5f);
-        // sequence[musicNotationToOffsetSample(3, 1)] = new MusicObjectInfo("Asteroid Field", 0.5f);
-        //sequence[musicNotationToOffsetSample(5, 1)] = new MusicObjectInfo("Asteroid Field", 0.5f);
-        //sequence[musicNotationToOffsetSample(7, 1)] = new MusicObjectInfo("Supernova", 0.5f);
-        sequence[musicNotationToOffsetSample(11, 1)] = new MusicObjectInfo("StarCluster1", 0.5f);
-        sequence[musicNotationToOffsetSample(15, 1)] = new MusicObjectInfo("StarCluster2", 0.5f);
+        // sequence[musicNotationToOffsetSample(3)] = new MusicObjectInfo("Asteroid Field", 0.5f);
+        //sequence[musicNotationToOffsetSample(5)] = new MusicObjectInfo("Asteroid Field", 0.5f);
+        //sequence[musicNotationToOffsetSample(7)] = new MusicObjectInfo("Supernova", 0.5f);
+        // sequence[musicNotationToOffsetSample(9)] = new MusicObjectInfo("Space Probe", 0.5f);
+        // sequence[musicNotationToOffsetSample(11)] = new MusicObjectInfo("StarClusterBig1", 0.5f);
+        sequence[musicNotationToOffsetSample(13)] = new MusicObjectInfo("StarBb2", 0.5f);
+        //sequence[musicNotationToOffsetSample(15)] = new MusicObjectInfo("StarClusterBig2", 0.5f);
+        // sequence[musicNotationToOffsetSample(17)] = new MusicObjectInfo("Space Probe", 0.5f);
         
 
         // Verse 1
         int verse1StartBar = 19;  // 19-26
         writeVerseStartingAtBar(verse1StartBar, 0.3f, 0.3f);
         writeVerseStartingAtBar(verse1StartBar + 4, 0.3f, 0.55f);
-        sequence[musicNotationToOffsetSample(verse1StartBar + 8, 1)] = new MusicObjectInfo("StarBb2", 0.58f);
+        sequence[musicNotationToOffsetSample(verse1StartBar + 8, 1)] = new MusicObjectInfo("StarBb2", 0.5f);
+
+        // Interlude 1
+        int interlude1StartBar = 27;  // 27-34
+        sequence[musicNotationToOffsetSample(interlude1StartBar)] = new MusicObjectInfo("StarCluster1", 0.5f);
+        // sequence[musicNotationToOffsetSample(interlude1StartBar + 2)] = new MusicObjectInfo("Space Probe", 0.5f);
+        // sequence[musicNotationToOffsetSample(interlude1StartBar + 4)] = new MusicObjectInfo("BlackHole", 0.5f);
+        // sequence[musicNotationToOffsetSample(interlude1StartBar + 6)] = new MusicObjectInfo("Space Probe", 0.5f);
 
         // Chorus 1
-        int chorus1StartBar = 27;  // 27-34
+        int chorus1StartBar = 35;  // 35-42
         writeChorusStartingAtBar(chorus1StartBar, 0.55f, 0.65f);
         writeChorusStartingAtBar(chorus1StartBar + 4, 0.65f, 0.75f);
 
+        // Interlude 2
+        int interlude2StartBar = 43;  // 43-50
+        sequence[musicNotationToOffsetSample(interlude2StartBar)] = new MusicObjectInfo("StarCluster1", 0.5f);
+        // sequence[musicNotationToOffsetSample(interlude2StartBar + 4)] = new MusicObjectInfo("BlackHole", 0.5f);
+
         // Verse 2
-        int verse2StartBar = 35;  // 35-42
+        int verse2StartBar = 51;  // 51-58
         writeVerseStartingAtBar(verse2StartBar, 0.3f, 0.3f);
         writeVerseStartingAtBar(verse2StartBar + 4, 0.3f, 0.55f);
-        sequence[musicNotationToOffsetSample(verse2StartBar + 8, 1)] = new MusicObjectInfo("StarBb2", 0.58f);
+        sequence[musicNotationToOffsetSample(verse2StartBar + 8)] = new MusicObjectInfo("StarBb2", 0.5f);
+        
+        // Chorus 2
+        int chorus2StartBar = 59;  // 59-66
+        writeChorusStartingAtBar(chorus2StartBar, 0.55f, 0.65f);
+        writeChorusStartingAtBar(chorus2StartBar + 4, 0.65f, 0.75f);
+        // sequence[musicNotationToOffsetSample(chorus2StartBar + 4)] = new MusicObjectInfo("Nebula", 0.5f);
+
+        // Bridge
+        int bridgeStartBar = 67;  // 67-82
+        sequence[musicNotationToOffsetSample(bridgeStartBar)] = new MusicObjectInfo("StarCluster2", 0.5f);
+        // sequence[musicNotationToOffsetSample(bridgeStartBar + 4)] = new MusicObjectInfo("Space Probe", 0.5f);
+        sequence[musicNotationToOffsetSample(bridgeStartBar + 8)] = new MusicObjectInfo("StarCluster2", 0.5f);
+        // sequence[musicNotationToOffsetSample(bridgeStartBar + 8)] = new MusicObjectInfo("Space Probe", 0.5f);
+        // sequence[musicNotationToOffsetSample(bridgeStartBar + 12)] = new MusicObjectInfo("WormHole", 0.5f);
+        // sequence[musicNotationToOffsetSample(bridgeStartBar + 14)] = new MusicObjectInfo("Space Probe", 0.5f);
+
+        // Chorus 3
+        int chorus3StartBar = 83;  // 83-90
+        writeChorusStartingAtBar(chorus3StartBar, 0.55f, 0.65f);
+        writeChorusStartingAtBar(chorus3StartBar + 4, 0.65f, 0.75f);
+        // sequence[musicNotationToOffsetSample(chorus3StartBar + 4)] = new MusicObjectInfo("Quasar", 0.5f);
+        // sequence[musicNotationToOffsetSample(interlude2StartBar + 6)] = new MusicObjectInfo("Space Probe", 0.5f);
+
+        // Outro
+        int outroStartBar = 91;  // 91-99
+        // sequence[musicNotationToOffsetSample(outroStartBar + 8)] = new MusicObjectInfo("Space Probe", 0.5f);
     }
 
     /* Input time at which the audio should be triggered, returns corresponding offset sample 
     bar, beat and sixteenth are 1-indexed. bar > 0, beat and sixteenth in [1, 4].
     offset is to compensate for the distance between spawning position and player position */
-    private static int musicNotationToOffsetSample(int bar, int beat, int sixteenth = 1)
+    private static int musicNotationToOffsetSample(int bar, int beat=1, int sixteenth=1)
     {
         // TODO: create assertion for bar, beat and sixteenth
         float timeInSeconds = (float)(bar - 1) * sixteenthTime * 16 + (float)(beat - 1) * sixteenthTime * 4 + (float)(sixteenth - 1) * sixteenthTime;
@@ -70,38 +107,41 @@ public class Sequence
         }
     }
 
-    /* Writes the two bars of verse melody starting from startBar on the Sequence, 
-    and applies linear brightness automation from start to end value */
-    private static void writeVerseStartingAtBar(int startBar, float brightnessAutomationStartValue, float brightnessAutomationEndValue)
+    /* Writes the four bars of verse melody starting from startBar on the Sequence, 
+    and applies linear automation of y-coordinate from start to end value */
+    private static void writeVerseStartingAtBar(int startBar, float yAutomationStartValue, float yAutomationEndValue)
     {
-        float brightnessRange = brightnessAutomationEndValue - brightnessAutomationStartValue;
+        float brightnessRange = yAutomationEndValue - yAutomationStartValue;
         float stepSize = 1f/5f;
-        sequence[musicNotationToOffsetSample(startBar, 1)] = new MusicObjectInfo("StarBb2", brightnessAutomationStartValue);
-        sequence[musicNotationToOffsetSample(startBar, 4)] = new MusicObjectInfo("StarAb2", brightnessAutomationStartValue + stepSize * brightnessRange);
-        sequence[musicNotationToOffsetSample(startBar + 1, 1)] = new MusicObjectInfo("StarBb2", brightnessAutomationStartValue + 2f * stepSize * brightnessRange);
-        sequence[musicNotationToOffsetSample(startBar + 2, 1)] = new MusicObjectInfo("StarEb3", brightnessAutomationStartValue + 3f * stepSize * brightnessRange);
-        sequence[musicNotationToOffsetSample(startBar + 3, 1)] = new MusicObjectInfo("StarC3", brightnessAutomationStartValue + 4f * stepSize * brightnessRange);
-        sequence[musicNotationToOffsetSample(startBar + 3, 4)] = new MusicObjectInfo("StarAb2", brightnessAutomationEndValue);
+        sequence[musicNotationToOffsetSample(startBar)] = new MusicObjectInfo("StarBb2", yAutomationStartValue);
+        sequence[musicNotationToOffsetSample(startBar, 4)] = new MusicObjectInfo("StarAb2", yAutomationStartValue + stepSize * brightnessRange);
+        sequence[musicNotationToOffsetSample(startBar + 1)] = new MusicObjectInfo("StarBb2", yAutomationStartValue + 2f * stepSize * brightnessRange);
+        sequence[musicNotationToOffsetSample(startBar + 2)] = new MusicObjectInfo("StarEb3", yAutomationStartValue + 3f * stepSize * brightnessRange);
+        sequence[musicNotationToOffsetSample(startBar + 3)] = new MusicObjectInfo("StarC3", yAutomationStartValue + 4f * stepSize * brightnessRange);
+        sequence[musicNotationToOffsetSample(startBar + 3, 4)] = new MusicObjectInfo("StarAb2", yAutomationEndValue);
     }
-    private static void writeChorusStartingAtBar(int startBar, float brightnessAutomationStartValue, float brightnessAutomationEndValue)
-    {
-        float brightnessRange = brightnessAutomationEndValue - brightnessAutomationStartValue;
-        float stepSize = 1f/13f;
-        sequence[musicNotationToOffsetSample(startBar, 1)] = new MusicObjectInfo("StarBb2", brightnessAutomationStartValue);
-        sequence[musicNotationToOffsetSample(startBar, 1, 4)] = new MusicObjectInfo("StarEb3", brightnessAutomationStartValue + stepSize * brightnessRange);
-        sequence[musicNotationToOffsetSample(startBar, 3)] = new MusicObjectInfo("StarF3", brightnessAutomationStartValue + 2f * stepSize * brightnessRange);
-        sequence[musicNotationToOffsetSample(startBar, 3, 4)] = new MusicObjectInfo("StarEb3", brightnessAutomationStartValue + 3f * stepSize * brightnessRange);
-        sequence[musicNotationToOffsetSample(startBar + 1, 1)] = new MusicObjectInfo("StarDb3", brightnessAutomationStartValue + 4f * stepSize * brightnessRange);
-        sequence[musicNotationToOffsetSample(startBar + 1, 1, 4)] = new MusicObjectInfo("StarBb2", brightnessAutomationStartValue + 5f * stepSize * brightnessRange);
 
-        sequence[musicNotationToOffsetSample(startBar + 2, 1)] = new MusicObjectInfo("StarF3", brightnessAutomationStartValue + 6f * stepSize * brightnessRange);
-        sequence[musicNotationToOffsetSample(startBar + 2, 1, 4)] = new MusicObjectInfo("StarAb3", brightnessAutomationStartValue + 7f * stepSize * brightnessRange);
-        sequence[musicNotationToOffsetSample(startBar + 2, 3)] = new MusicObjectInfo("StarF3", brightnessAutomationStartValue + 8f * stepSize * brightnessRange);
-        sequence[musicNotationToOffsetSample(startBar + 2, 3, 4)] = new MusicObjectInfo("StarEb3", brightnessAutomationStartValue + 9f * stepSize * brightnessRange);
-        sequence[musicNotationToOffsetSample(startBar + 3, 1)] = new MusicObjectInfo("StarDb3", brightnessAutomationStartValue + 10f * stepSize * brightnessRange);
-        sequence[musicNotationToOffsetSample(startBar + 3, 1, 4)] = new MusicObjectInfo("StarBb2", brightnessAutomationStartValue + 11f * stepSize * brightnessRange);
-        sequence[musicNotationToOffsetSample(startBar + 3, 3)] = new MusicObjectInfo("StarAb2", brightnessAutomationStartValue + 12f * stepSize * brightnessRange);
-        sequence[musicNotationToOffsetSample(startBar + 3, 3, 4)] = new MusicObjectInfo("StarBb2", brightnessAutomationEndValue);
+    /* Writes the four bars of chorus melody starting from startBar on the Sequence, 
+    and applies linear automation of y-coordinate from start to end value */
+    private static void writeChorusStartingAtBar(int startBar, float yAutomationStartValue, float yAutomationEndValue)
+    {
+        float brightnessRange = yAutomationEndValue - yAutomationStartValue;
+        float stepSize = 1f/13f;
+        sequence[musicNotationToOffsetSample(startBar)] = new MusicObjectInfo("StarBb2", yAutomationStartValue);
+        sequence[musicNotationToOffsetSample(startBar, 1, 4)] = new MusicObjectInfo("StarEb3", yAutomationStartValue + stepSize * brightnessRange);
+        sequence[musicNotationToOffsetSample(startBar, 3)] = new MusicObjectInfo("StarF3", yAutomationStartValue + 2f * stepSize * brightnessRange);
+        sequence[musicNotationToOffsetSample(startBar, 3, 4)] = new MusicObjectInfo("StarEb3", yAutomationStartValue + 3f * stepSize * brightnessRange);
+        sequence[musicNotationToOffsetSample(startBar + 1)] = new MusicObjectInfo("StarDb3", yAutomationStartValue + 4f * stepSize * brightnessRange);
+        sequence[musicNotationToOffsetSample(startBar + 1, 1, 4)] = new MusicObjectInfo("StarBb2", yAutomationStartValue + 5f * stepSize * brightnessRange);
+
+        sequence[musicNotationToOffsetSample(startBar + 2)] = new MusicObjectInfo("StarF3", yAutomationStartValue + 6f * stepSize * brightnessRange);
+        sequence[musicNotationToOffsetSample(startBar + 2, 1, 4)] = new MusicObjectInfo("StarAb3", yAutomationStartValue + 7f * stepSize * brightnessRange);
+        sequence[musicNotationToOffsetSample(startBar + 2, 3)] = new MusicObjectInfo("StarF3", yAutomationStartValue + 8f * stepSize * brightnessRange);
+        sequence[musicNotationToOffsetSample(startBar + 2, 3, 4)] = new MusicObjectInfo("StarEb3", yAutomationStartValue + 9f * stepSize * brightnessRange);
+        sequence[musicNotationToOffsetSample(startBar + 3)] = new MusicObjectInfo("StarDb3", yAutomationStartValue + 10f * stepSize * brightnessRange);
+        sequence[musicNotationToOffsetSample(startBar + 3, 1, 4)] = new MusicObjectInfo("StarBb2", yAutomationStartValue + 11f * stepSize * brightnessRange);
+        sequence[musicNotationToOffsetSample(startBar + 3, 3)] = new MusicObjectInfo("StarAb2", yAutomationStartValue + 12f * stepSize * brightnessRange);
+        sequence[musicNotationToOffsetSample(startBar + 3, 3, 4)] = new MusicObjectInfo("StarBb2", yAutomationEndValue);
     }
 }
 
