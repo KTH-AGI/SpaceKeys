54854505706ed9bc43dafa927de8eae19d5253b1
Sofia Bergqvist <sofiabergqvist93@gmail.com>
2023-12-14 17:11:16 +0100

    Feat: Add keyboard interaction for the UFOs (#104)
    added keyboard interactions such that you control the left UFO with WASD and the right UFO with IJKL

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    private Vector2 _input;
+    private CharacterController _characterController;
+    private Vector3 _direction;
+
+    [SerializeField] private float speed;
+
+    private void Awake()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        _characterController.Move(_direction * speed * Time.deltaTime);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        _input = context.ReadValue<Vector2>();
+        _direction = new Vector3(_input.x, _input.y, 0f);
+    }
+} 
