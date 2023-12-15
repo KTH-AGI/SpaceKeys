98b469f27c495d1491466bf17658743667d02685
xxpermanentxx <shiqi.gong.2000@gmail.com>
2023-12-15 01:52:15 +0100

    Fixed bug and the UI of End Screen

--- Assets/Scripts/Logic/GameManager.cs
+++ Assets/Scripts/Logic/GameManager.cs
@@ -26,6 +26,7 @@ public class GameManager : MonoBehaviour
     private PauseScreenLayers pauseScreenLayers;
     
     private int comboCount = 0; // Current combo count
+    private int maxCombo = 0; // Maximum combo count
     private float scoreMultiplier = 1.0f; // Score multiplier
     private int score = 0; // Current score
     
@@ -37,12 +38,12 @@ public class GameManager : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject);
+            Debug.Log("GameManager Created");
         }
         else if (Instance != this)
         {
+            Debug.Log("GameManager already exists, destroying duplicate.");
             Destroy(gameObject);
-            return;
         }
         
         // Subscribe to the OnFistDetected event of the PlayerController
@@ -54,6 +55,7 @@ public class GameManager : MonoBehaviour
         NoteLight.OnMissNote += OnMissNote;
     }
     
+
     private void OnDestroy()
     {
         // Unsubscribe from the OnFistDetected event to prevent memory leaks
@@ -66,6 +68,8 @@ public class GameManager : MonoBehaviour
             Destroy(Instance.gameObject);
             Instance = null;
         }
+        MusicObject.OnCollisionNote -= OnCollisionNote;
+        NoteLight.OnMissNote -= OnMissNote;
     }
     
     // Be called when the OnFistDetected event is triggered
@@ -131,6 +135,14 @@ public class GameManager : MonoBehaviour
         Time.timeScale = 1;
         SceneManager.LoadScene("HomeScene");
     }
+    
+    public void LoadEndScene()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("EndScene");
+        PlayerPrefs.SetInt("Score", score);
+        PlayerPrefs.SetInt("MaxCombo", maxCombo);
+    }
 
     private void OnCollisionNote(Vector3 notePos, Vector3 playerPos, float radius)
     {
@@ -153,24 +165,27 @@ public class GameManager : MonoBehaviour
         else if (distance < radius)
         {
             UpdateScoreAndCombo("Orbital", 0.2f, notePos, resetCombo: true);
-            ResetCombo();
         }
     }
     
     private void OnMissNote(Vector3 notePos)
     {
         UpdateScoreAndCombo("Miss", 0, notePos, resetCombo: true);
-        ResetCombo();
     }
     
     private void UpdateScoreAndCombo(string hitQuality, float scoreFactor, Vector3 notePosition, bool resetCombo = false)
     {
         if (resetCombo)
         {
+            if (comboCount > maxCombo)
+            {
+                maxCombo = comboCount;
+            }
             comboCount = 0;
             scoreMultiplier = 1.0f;
             score += (int)(100 * scoreFactor);
             imageLayers.ShowHitQualityImage(notePosition, hitQuality);
+            
         }
         else
         {
@@ -193,12 +208,5 @@ public class GameManager : MonoBehaviour
         pointTextLayers.UpdateScoreIncrementally(score);
         
         comboTextLayers.UpdateCombo(comboCount);
-    }
-    
-    private void ResetCombo()
-    {
-        comboCount = 0;
-        scoreMultiplier = 1.0f;
-        UpdateUI();
     }
 }
